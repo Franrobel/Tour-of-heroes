c92df7bf92da3ded9468c9bd8b776fefd7184cdd
Francisco Rodriguez <franrobelluati@gmail.com>
2022-08-08 17:41:35 +0200

    add heroservice and get heroes in heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'heroes',
   templateUrl: './heroes.component.html',
@@ -9,21 +10,25 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   
 
-heroes = HEROES 
-selectedHero?: Hero
+  heroes: Hero[] = [];
+  selectedHero?: Hero
 
 
 
-constructor() { }
+constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
    // console.log('donde esta ', this.selectedHero)
-   console.log('heroes outside,', this.heroes);
-   console.log('selectedHero outside,', this.selectedHero);
-
+ //  console.log('heroes outside,', this.heroes);
+   //console.log('selectedHero outside,', this.selectedHero);
+this.getHeroes()
 
   }
-  
+  getHeroes(): void {
+                  
+ this.heroService.getHeroes()
+ .subscribe(heroes => this.heroes = heroes);
+  }
   onSelect(hero: Hero): void {
     console.log('this.selectedHero before', this.selectedHero)
     this.selectedHero = hero;
