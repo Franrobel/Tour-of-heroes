c3142b1ac04901c58921692f4361d8721511d148
Francisco Rodriguez <franrobelluati@gmail.com>
2022-08-08 14:06:29 +0200

    add heroes-details-component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-hero: Hero = {
-  id: 1, 
-  name: 'windstorm'
-}
+  
+
+heroes = HEROES 
+selectedHero?: Hero
+
+
 
 constructor() { }
 
   ngOnInit(): void {
-  }
+   // console.log('donde esta ', this.selectedHero)
+   console.log('heroes outside,', this.heroes);
+   console.log('selectedHero outside,', this.selectedHero);
 
+
+  }
+  
+  onSelect(hero: Hero): void {
+    console.log('this.selectedHero before', this.selectedHero)
+    this.selectedHero = hero;
+    console.log('this.selectedHero after', this.selectedHero)
+  }
 }
